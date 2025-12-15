9c237f54e8743aed2854ebdfacea332aae0a1c6e
vrajdhoriyaniNITK <vraj.dhoriyani1107@gmail.com>
2025-12-15 16:18:42 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask , request
 import os
 from datetime import datetime as dt
+import sqlite3
 
 webapp = Flask(__name__)
 uploads = "evidence001"
@@ -10,12 +11,36 @@ if not os.path.exists(uploads):
     os.mkdir(uploads)
 
 
+db_name = "audit_logs.db"
+
+
+def startdb():
+    connect1 = sqlite3.connect(db_name)
+    cursor1 = connect1.cursor()
+
+
+    cursor1.execute("""
+    CREATE TABLE IF NOT EXISTS events( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    device_id TEXT,
+    timestamp TEXT,
+    evidence_path TEXT,
+    ip_address TEXT) """
+    )
+
+    connect1.commit()
+    connect1.close()
+
+startdb()
+
 @webapp.route("/")
 def home():
     return "Server running"
 
 @webapp.route("/uploads", methods=["POST"])
+
 def upload():
+    device_id = request.form.get("device_id", "UNKNOWN")
     #get file
     img = request.files.get("img")
 
@@ -27,9 +52,26 @@ def upload():
     file_name = f"{timestamp}.jpg"
 
 
-    img.save(os.path.join(uploads,file_name))
+    save_path = os.path.join(uploads,file_name)
+    img.save(save_path)
 
-    return {"status": "recieved","filename": file_name}
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO events(device_id,timestamp,evidence_path,ip_address) VALUES(?,?,?,?) """,
+    (device_id,timestamp,save_path,request.remote_addr)
+
+
+
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {"status": "received","filename": file_name}
+    
+
 
 
 if __name__ == "__main__":
